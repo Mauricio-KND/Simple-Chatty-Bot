15407d9d468272121afb53ce7fba5f67ce291b1d
Mauricio-KND <1299@holbertonschool.com>
2021-01-17 23:16:40 -0500

    first commit

---
+++ Problems/Javadoc comment/src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    /**
+     * It prints the text "It works correctly" to the standard output
+     *
+     * @param args from the command line
+     */
+    public static void main(String[] args) {
+        System.out.println("It works correctly");
+    }
+}
