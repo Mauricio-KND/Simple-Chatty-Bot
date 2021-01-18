d8af44a21d69f79119ffa69a8bc26506b57e3dea
Mauricio-KND <1299@holbertonschool.com>
2021-01-18 00:22:09 -0500

    Adding more files

--- Problems/Hiring a chef/src/Main.java
+++ Problems/Hiring a chef/src/Main.java
@@ -1,7 +1,19 @@
 //put imports you need here
 
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        // put your code here
+        //Create an object of Scanner class, to use its methods.
+        //System.in indicates that the program read text typed in the standard input.
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+        scanner.next();
+        scanner.next();
+        scanner.next();
+        String cuisinePreferences = scanner.next();
+
+        System.out.println("The form for "+firstName+" is completed. We will contact you if we need a chef that cooks "+cuisinePreferences+" dishes.");
     }
 }
