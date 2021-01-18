3970d8a340efc73090725137854493a04cda5e98
Mauricio-KND <1299@holbertonschool.com>
2021-01-18 12:11:37 -0500

    Commiting Printing and Scanning Section

--- Problems/Guest list of a hotel/src/Main.java
+++ Problems/Guest list of a hotel/src/Main.java
@@ -1,7 +1,31 @@
 //put imports you need here
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+        String fourthWord = scanner.next();
+        String fifthWord = scanner.next();
+        String sixthWord = scanner.next();
+        String seventhWord = scanner.next();
+        String eightWord = scanner.next();
+
+        System.out.println(eightWord);
+        System.out.println(seventhWord);
+        System.out.println(sixthWord);
+        System.out.println(fifthWord);
+        System.out.println(fourthWord);
+        System.out.println(thirdWord);
+        System.out.println(secondWord);
+        System.out.println(firstWord);
+
+
+
     }
 }
