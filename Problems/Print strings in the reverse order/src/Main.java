8e2c62380c11905e27a20835f554eac921b8db23
Mauricio-KND <1299@holbertonschool.com>
2021-01-18 11:50:39 -0500

    Commiting Printing and Scanning Section

--- Problems/Print strings in the reverse order/src/Main.java
+++ Problems/Print strings in the reverse order/src/Main.java
@@ -1,7 +1,18 @@
 //put imports you need here
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        String firstLine = scanner.nextLine();
+        String secondLine = scanner.nextLine();
+        String thirdLine = scanner.nextLine();
+
+        System.out.println(thirdLine);
+        System.out.println(secondLine);
+        System.out.println(firstLine);
     }
 }
