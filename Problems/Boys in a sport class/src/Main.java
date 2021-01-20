4b80bdd709183c2bb804ce755dc71be4bd8d83c8
Mauricio-KND <1299@holbertonschool.com>
2021-01-20 01:45:14 -0500

    Second Commit of today

---
+++ Problems/Boys in a sport class/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int h1 = scanner.nextInt();
+        int h2 = scanner.nextInt();
+        int h3 = scanner.nextInt();
+
+        boolean ascending = h1 <= h2 && h2 <= h3;
+        boolean descending = h1 >= h2 && h2 >= h3;
+
+        boolean finalChecker = ascending || descending;
+
+        System.out.println(finalChecker);
+    }
+}
