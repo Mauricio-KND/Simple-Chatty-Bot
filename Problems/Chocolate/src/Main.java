ba76dbc699e3b2e0947bec2caa47f70897d31fbe
Mauricio-KND <1299@holbertonschool.com>
2021-01-21 00:41:53 -0500

    Commiting new problems

--- Problems/Chocolate/src/Main.java
+++ Problems/Chocolate/src/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int firstInteger = scanner.nextInt();
-        int secondInteger = scanner.nextInt();
-        int thirdInteger = scanner.nextInt();
+        int intN = scanner.nextInt();
+        int intM = scanner.nextInt();
+        int intK = scanner.nextInt();
 
-
-
+        if (intK % intN == 0 && intK / intN < intM || intK % intM == 0 && intK / intM < intN) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
